9dcc6e1f2251257e878faf16090cccd1b31b6ff3
zacharykratochvil <zacharykratochvil@gmail.com>
2023-05-27 07:44:35 -0400

    semi-working cpp ultrasonic publisher

--- src/ultrasonic_publisher.cpp
+++ src/ultrasonic_publisher.cpp
@@ -19,139 +19,136 @@
 // message imports
 #include "freenove_ros/SensorDistance.h"
 
-/*
-* code partially from original Freenove code
-* otherwise by Zachary Kratochvil
-*/
-
-class Ultrasonic{
-	private:
-		unsigned smoothing = 5;
-		unsigned trigger_pin = 27;
-	    unsigned echo_pin = 22;
+// ultrasonic sensing and publishing logic
+class Ultrasonic_ROS{
 
 	public:
-	    Ultrasonic(){
-	        /*
-	        if(new_smoothing != NULL){
-	        	smoothing = *new_smoothing;
-	        };
-	        */
-	        smoothing = 5;
+		static const unsigned CHANGE_LOW = 0;
+    	static const unsigned CHANGE_HIGH = 1;
+    	static const unsigned TIMEOUT = 2;
 
-	        // intialize GPIO
+		static const unsigned trigger_pin = 27;
+	    static const unsigned echo_pin = 22;
+
+	    static inline unsigned smoothing;
+	    static inline std::vector<double> distances_cm;
+
+	    static inline double start_time;
+
+		static void initialize(unsigned smoothing_param){
+			// initialize class params
+			smoothing = smoothing_param;
+			distances_cm.assign(smoothing, -1);
+			start_time = std::numeric_limits<double>::max();
+
+			// intialize GPIO
 	        if (gpioInitialise() < 0){
-			   //rospy.logerr("GPIO initialization failed.");
+			   ROS_ERROR("%s", "Failed to initialize GPIO.");
 			};
 			gpioSetMode(trigger_pin, PI_OUTPUT);
 			gpioSetMode(echo_pin, PI_INPUT);
+
+			gpioSetAlertFunc(echo_pin, &process_distance);
 		};
 
-	    void send_trigger_pulse(){
-	        gpioWrite(trigger_pin, 1);
-	        sleep(0.00015);
-	        gpioWrite(trigger_pin, 0);
-	    };
-	     
-	    // take median of self.smoothing many measurements
-	    double get_distance(){
-	    	vector<double> distance_cm = vect(smoothing,-1);
-	        for(unsigned i = 0; i < std::vector::size(distance_cm); i++){}
-	            
-	            # take and store measurement
-	            self.send_trigger_pulse()
-	            
-	            timeout = GPIO.wait_for_edge(self.echo_pin, GPIO.RISING, timeout=35)
-	            if timeout is None:
-	                continue
-	            start = time.time()
-	            
-	            timeout = GPIO.wait_for_edge(self.echo_pin, GPIO.FALLING, timeout=35)
-	            if timeout is None:
-	                continue
-	            finish = time.time()
-
-	            pulse_len = finish-start
-	            distance_cm[i] = pulse_len/0.000058
-	        }
-	        
-	        # not accurate above 5m
-	        valids = np.nonzero((distance_cm > 0) & (distance_cm < 500))[0]
-	        if len(valids) == 0:
-	            return -1
-	        else:
-	            distance_cm=sorted(distance_cm[valids])
-	            middle = len(distance_cm) // 2
-	            median = distance_cm[middle]
-	            return median
-	            */
-};
-          
-  
-  /*      
-// Test logic follows:
-int main(){
-
-    cout << "Program is starting ... ";
-    
-    Ultrasonic ultrasonic=Ultrasonic(NULL);
-    ultrasonic.send_trigger_pulse();
-    while(true){
-            //cout << ultrasonic.get_distance());
-            sleep(10);
-    };
-
-    return 0;
-};
-*/
-
-
-class Ultrasonic_ROS{
-
-	private:
-		ros::NodeHandle n;
-		ros::Publisher ultraPub;
-		Ultrasonic sensor;
-
-	public:
-		Ultrasonic_ROS(){
-			ultraPub = n.advertise<freenove_ros::SensorDistance>("ultrasonic_distance", 1);
-
-			sensor = Ultrasonic();
-		};
-
-		double get_seconds(){
-			auto current_time = std::chrono::system_clock::now();
-			auto duration = std::chrono::duration<double>(current_time.time_since_epoch());
-			return duration.count();
-		};
-
-		void start(unsigned rate){
+		static void start(unsigned rate){
 
 			ROS_ERROR("%s", "ready to publish");
 
 			ros::Rate ros_rate = ros::Rate(rate);
 			
-			// publish
+			// measure
 			while (ros::ok()){
 
 				ROS_ERROR("%s", "measuring");
 				
-				double start = get_seconds();
-				double distance = sensor.get_distance();
-				double end = get_seconds();
-				
-				double avg_time = (start+end)/2;
-				ros::Time timestamp = ros::Time(avg_time);
-				
-				freenove_ros::SensorDistance msg = freenove_ros::SensorDistance();
-				msg.cm = 0; //distance;
-				msg.timestamp = timestamp;
-				ultraPub.publish(msg);
-
+				send_trigger_pulse();
+			
 				ros::spinOnce();
 				ros_rate.sleep();
 			};
+		};
+
+	    static void send_trigger_pulse(){
+	        gpioWrite(trigger_pin, 1);
+	        sleep(0.00015);
+	        gpioWrite(trigger_pin, 0);
+	    };
+
+	    static void process_distance(int pin, int level, unsigned int tick){
+
+	    	switch(level){
+	    		case CHANGE_HIGH:
+	    		{
+	    			start_time = get_seconds();
+	    			return;
+    			}
+	    		case CHANGE_LOW:
+    			{
+	    			double end = get_seconds();
+	    			double pulse_len = end - start_time;
+	    			push(&distances_cm, pulse_len/0.000058);
+	    			break;
+	    		}
+	    		case TIMEOUT:
+	    			push(&distances_cm, 0);
+	    			start_time = std::numeric_limits<double>::max();
+	    			break;
+	    	};
+
+	    	std::vector<unsigned short> valids;
+	    	valids.assign(smoothing, 1);
+	    	unsigned num_valids = smoothing;
+	    	for(unsigned i = 0; i < smoothing; i++){
+	    		if(distances_cm.at(i) < 0){
+	    			valids.at(i) = 0;
+	    			num_valids--;
+	    		};
+	    	};
+
+
+	    	if(num_valids == 0){
+	            //currently silent when no valid data
+	        }
+	        else{
+	        	for(unsigned i = 0; i < distances_cm.size(); i++){
+	        		distances_cm.at(i) = distances_cm.at(i)*valids.at(i);
+	        	}
+	        	std::sort(distances_cm.begin(), distances_cm.end());
+
+	        	unsigned i;
+	        	for(; i <= distances_cm.size(); i++){
+	        		if(distances_cm.at(i) > 0){
+	        			break;
+	        		};
+	        	};
+
+	            unsigned middle = std::floor((i + distances_cm.size())/2);
+	            double median = distances_cm.at(middle);
+	            publish(median);
+	        };  
+	    };
+
+	    static void push(std::vector<double> *v, double val){
+	    	for(unsigned i = smoothing-1; i >= 0; i--){
+				v->at(i) = v->at(i - 1);
+			};
+			v->at(0) = val;
+	    };
+
+		static double get_seconds(){
+			auto current_time = std::chrono::system_clock::now();
+			auto duration = std::chrono::duration<double>(current_time.time_since_epoch());
+			return duration.count();
+		};
+
+		static void publish(double distance_cm){
+			ros::Time timestamp = ros::Time(get_seconds());
+				
+			freenove_ros::SensorDistance msg = freenove_ros::SensorDistance();
+			msg.cm = distance_cm;
+			msg.timestamp = timestamp;
+			//ultraPub.publish(msg);
 		};
 };
 		
@@ -165,9 +162,19 @@ class Ultrasonic_ROS{
 int main(int argc, char **argv){
 
 	ros::init(argc, argv, "ultrasonic_publisher");
+	ros::NodeHandle n;
+	//ultraPub = n.advertise<freenove_ros::SensorDistance>("ultrasonic_distance", 1);
 
-    Ultrasonic_ROS sensor = Ultrasonic_ROS();
-    sensor.start(100);
+	/*
+    try{
+    	Ultrasonic_ROS::initialize(5);
+    	Ultrasonic_ROS::start(100);
+    }
+    catch(int e){
+    	gpioTerminate();
+    	return 1;
+    }
 
+*/
     return 0;
 };
